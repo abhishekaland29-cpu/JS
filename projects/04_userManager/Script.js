4924139b18fc80be0a9661d95d26a7fa5bd515f1
Abhishek Aland <abhishekaland29@gmail.com>
2026-09-17 23:59:42 +0530

    Modified 04_userManager by adding remove user feature.

--- projects/04_userManager/Script.js
+++ projects/04_userManager/Script.js
@@ -4,73 +4,78 @@ let username = document.querySelector("#name");
 let bio = document.querySelector("#bio");
 let role = document.querySelector("#role");
 
-
 const userManager = {
     users: [],
+
     init: function () {
         form.addEventListener("submit", this.submitForm.bind(this));
     },
+
     submitForm: function (e) {
         e.preventDefault();
-        console.log("Form Submitted.");
+
         this.addUser();
         form.reset();
     },
+
     addUser: function () {
         this.users.push({
             username: username.value,
             role: role.value,
             bio: bio.value,
-            image: photo.value,
+            image: photo.value
         });
+
         this.renderUI();
     },
+
     renderUI: function () {
+        let container = document.querySelector(".cards-container");
+        container.innerHTML = "";
 
-        document.querySelector(".cards-container").innerHTML= " ";
+        this.users.forEach((user, index) => {
 
-        this.users.forEach(function (user) {
             const card = document.createElement("div");
             card.className = "user-card";
 
             const image = document.createElement("img");
             image.className = "user-image";
-            image.id = "photo";
             image.src = user.image;
-            image.alt = "John Doe";
 
             const name = document.createElement("h2");
             name.className = "name";
-            name.id = "name";
             name.textContent = user.username;
 
             const role = document.createElement("p");
             role.className = "role";
-            role.id = "role";
             role.textContent = user.role;
 
             const bio = document.createElement("p");
             bio.className = "bio";
-            bio.id = "bio";
             bio.textContent = user.bio;
 
             const remove = document.createElement("button");
             remove.className = "remove-btn";
-            remove.id = "remove-btn";
             remove.textContent = "Remove User";
+
+            remove.addEventListener("click", () => {
+                this.removeUser(index);
+            });
 
             card.appendChild(image);
             card.appendChild(name);
             card.appendChild(role);
             card.appendChild(bio);
             card.appendChild(remove);
-            
-            document.querySelector(".cards-container").appendChild(card);
-        })
-        
+
+            container.appendChild(card);
+        });
     },
 
-    removeUser: function () {},
+    removeUser: function (index) {
+        this.users.splice(index, 1);
+        this.renderUI();
+    }
 };
 
 userManager.init();
